66138b7e583dd2716e72f7abcf3b66477a390810
Layne Silveira <laynepsilveira@gmail.com>
2023-08-20 14:54:35 -0300

    feature: normaling function

--- norm.py
+++ norm.py
@@ -1,37 +1,44 @@
 import numpy as np
 
-def norm_color(img):
-  max, min = max_min_color(img)
+def norm(img):
+  print(img.dtype)
+  max_img, min_img = max_min(img)
   new_img = []
   for line in img:
     aux_line = []
     for pixel in line:
       aux_pixel = []
       for i in range(3):
-        # if pixel[i] < 0 or pixel[i] > 255:
-        #   new_color = round((255 / (max[i] - min[i])) * (pixel[i] - min[i]))
-        #   aux_pixel.append(new_color)
-        if pixel[i] < 0:
-          aux_pixel.append(0)
-        elif pixel[i] > 255:
-          aux_pixel.append(255)
+        if pixel[i] < 0 or pixel[i] > 255:
+          new_pixel = np.round((255 / (max_img[i] - min_img[i])) * (pixel[i] - min[i]))
+          aux_pixel.append(new_pixel)
         else:
-          aux_pixel.append(round(pixel[i]))
+          aux_pixel.append(np.round(pixel[i]))
       aux_line.append(aux_pixel)
     new_img.append(aux_line)
 
-  return np.array(new_img)
+  new_img = np.array(new_img, dtype=img.dtype)
+  print(new_img.dtype)
+  return new_img
 
-def max_min_color(img):
-  red = []
-  green = []
-  blue = []
+def trunc(img):
+  shape = img.shape
+  img = img.flatten()
+  trunc = []
+  for px in img:
+    if px > 255:
+      trunc.append(255)
+    elif px < 0:
+      trunc.append(0)
+    else:
+      trunc.append(px)
+  trunc = np.array(trunc, np.int32)
+  return np.reshape(trunc, shape)
 
-  for line in img:
-    for pixel in line:
-      red.append(pixel[0])
-      green.append(pixel[1])
-      blue.append(pixel[2])
+def max_min(img):
+  red = img[:,:,2].flatten()
+  green = img[:,:,1].flatten()
+  blue = img[:,:,0].flatten()
 
   max_img = [max(red),
              max(green),
